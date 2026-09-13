d8a86739caaada1944707dbc1f0a5f5f6881eea5
iamguystephane <forlemustephane@gmail.com>
2026-09-13 20:10:48 +0100

    done with the app

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -149,7 +149,7 @@ export function Header() {
         <div className="mx-auto flex h-17 max-w-300 items-center justify-between px-5">
           {/* Logo */}
           <a href="/" aria-label="Federalogistic home" className="flex items-center select-none">
-            <img src="/full-logo.png" alt="Federalogistic" className="h-11 w-auto object-contain" />
+            <img src="/full-logo.png" alt="Federalogistic" className="h-15 w-auto object-contain" />
           </a>
 
           {/* Desktop links */}
